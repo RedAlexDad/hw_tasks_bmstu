5371c974b28ebd066bc458724091b87ae804000c
Alexey <boss6852@gmail.com>
2024-10-16 13:03:20 +0300

    Fix models and unittest

--- main.py
+++ main.py
@@ -12,27 +12,10 @@ from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_sc
 from sklearn.metrics import mean_squared_error
 from sklearn.pipeline import make_pipeline
 
-from CTDRNN import CTDRNN, CTDRNN_Model
+from CTDRNN import CTDRNN
 
 if __name__ == '__main__':
     df = pd.read_csv('Amp_C_train.txt')
-
-    # Приведение к нижнему регистру названия колонки
-    df.columns = df.columns.str.lower()
-
-    # Установка колонки 'time' в качестве индекса
-    df.set_index('time', inplace=True)
-
-    df['input'] = df['input'].apply(lambda x: complex(x.strip('()').replace('i', 'j')))
-    df['output'] = df['output'].apply(lambda x: complex(x.strip('()').replace('i', 'j')))
-
-    df['input_real'] = df['input'].apply(np.real)
-    df['input_imag'] = df['input'].apply(np.imag)
-    df['output_real'] = df['output'].apply(np.real)
-    df['output_imag'] = df['output'].apply(np.imag)
-
-    df = df.drop(['input', 'output'], axis=1)
-
     # Проверка наличия CUDA
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -47,74 +30,39 @@ if __name__ == '__main__':
         print("CUDA is not available. Running on CPU.")
 
     # Параметры задержки p
-    p=30 # Входная задержка
+    p=25 # Входная задержка
     q=15  # Выходная задержка
 
     # Создадим объект CTRNN
     ctdrnn = CTDRNN(
+        df=df,
         input_size=2 * (p + 1 + q), 
-        hidden_sizes=[128, 256, 256, 128, 64],
+        # hidden_layers=[2**4, 2**6, 2**8, 2**6, 2**4],
+        hidden_layers=[2**8, 2**10, 2**12, 2**10, 2**8],
+        # hidden_layers=[2**4, 2**4],
         output_size=2, 
-        num_layers=2,
-        num_epochs=10,
-        learning_rate=0.001,
+        epochs=10,
+        learning_rate=1e-4,
         p=p,
         q=q,
         batch_size=1024*1
     )
-
     # Вывод параметров в виде HTML-таблицы
     ctdrnn.print_params()
-
-    # Подготовка данных
-    X_seq, Y_seq = ctdrnn.prepare_data(df)
-
     # Обучим модель
-    ctdrnn.train()
-
+    # ctdrnn.train()
+    ctdrnn.train_and_save_plots()
+    # Сохраним историю обучения
+    ctdrnn.save_training_history()
     # График обучения
-    ctdrnn.plot_loss()
-
-    # Сделаем предсказания
-    # X_seq - входные данные для предсказания
-    Y_pred = ctdrnn.predict(X_seq)
-
-    # Создаем DataFrame из предсказаний
-    df_predictions = pd.DataFrame(Y_pred, columns=['pred_real', 'pred_imag'])
-
-    # Добавляем столбец с индексом (если необходимо)
-    df_predictions['index'] = np.arange(len(Y_pred))
-
-    # Сохраняем в CSV
-    df_predictions.to_csv('predictions_train.csv', index=False)
-
-    # Оценка модель
-    # Y_seq - реальные выходные данные
-    rmse_real, rmse_imag = ctdrnn.evaluate(Y_seq, Y_pred)
-
-    print(f'RMSE (Real part): {rmse_real}')
-    print(f'RMSE (Imaginary part): {rmse_imag}')
-    # Здесь будем сохранить результаты обучения
-    results = pd.DataFrame()
-    # А это будет счетчтиком для нумерация моеделй
-    count_model = 0
-    # Сохранение результатов
-    results[count_model] = ctdrnn.save_results(Y_seq, Y_pred, rmse_real, rmse_imag)
-
-    # display(results)
-    count_model += 1
-
-    # Сохраним модель
-    ctdrnn.save_model('ctdrnn') 
-
-    # Построим графики
-    info_result = pd.DataFrame({
-        'default_real': Y_seq[:, 0],  # Реальные значения (вещественная часть)
-        'default_imag': Y_seq[:, 1],  # Реальные значения (мнимая часть)
-        'pred_real': Y_pred[:, 0],    # Предсказанные значения (вещественная часть)
-        'pred_imag': Y_pred[:, 1]     # Предсказанные значения (мнимая часть)
-    })
-
-    info_result.index = range(len(Y_seq))  # Пример временной шкалы: индексы от 0 до количества наблюдений
-    ctdrnn.plot_predictions_vs_real(info_result, time_start=0, time_end=1.005e2)
-
+    ctdrnn.plot_training_history()
+    # Оценка модели
+    rmse_real, rmse_imag, predictions = ctdrnn.evaluate()
+    print(f"Evaluation RMSE (Real): {rmse_real:.6f}")
+    print(f"Evaluation RMSE (Imaginary): {rmse_imag:.6f}")
+    # Сохранение обученной модели
+    ctdrnn.save_model_pt()
+    # Сохраним предсказанные значения
+    ctdrnn.save_prediction(predictions)
+    # Графики
+    ctdrnn.plot_predictions(time_start=3.005e-4, time_end=3.006e-4)
